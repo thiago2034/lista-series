b53494f31d2161fedd36b3879817c0cf009333c7
Thiago Rodrigues de Araujo <40403082+thiago2034@users.noreply.github.com>
2020-04-07 22:44:02 -0300

    fix dist

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.use("/", serveStatic(path.join(__dirname, "/dist/spa")));
 
 const port = process.env.PORT || 8080;
 app.listen(port);
